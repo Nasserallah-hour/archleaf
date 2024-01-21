975b93a0a765227917864354e1707d7be5047789
nasserallah <nasserallah.hourichi@gmail.com>
2024-01-21 14:04:39 +0100

    added app to kubernetees

--- acrh/src/test/java/tn/atch/acrh/AcrhApplicationTests.java
+++ acrh/src/test/java/tn/atch/acrh/AcrhApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AcrhApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
